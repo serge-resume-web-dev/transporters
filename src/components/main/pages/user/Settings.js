21d98d7ecc182f5b6ac883998d7288644ace2f90
Serge <protopulos.pulatikos@yandex.ru>
2019-06-30 18:05:49 +0300

    settings page - fetch data, render fetched data in table

--- src/components/main/pages/user/Settings.js
+++ src/components/main/pages/user/Settings.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Settings = () => {
+	const [data, setData] = useState([]);
+	const url =
+		"http://5d0f6dddc56e7600145a42a6.mockapi.io/api/v1/users-database";
+
+	useEffect(() => {
+		async function fetchUsers() {
+			const response = await fetch(url);
+			response
+				.json()
+				.then(response => setData(response))
+				.catch(error => console.log("Error: ", error));
+		}
+		fetchUsers();
+	});
+
 	return (
 		<div>
-			<h3>User Settings Page</h3>
-			<ul>
-				<li>Paremeter - A</li>
-				<li>Paremeter - B</li>
-				<li>Paremeter - C</li>
-			</ul>
-			<div className="users-data">
+			<div className="users-data jumbotron">
 				<h3 className="text-center">Registered users</h3>
 				<table className="table">
 					<thead>
@@ -19,7 +28,15 @@ const Settings = () => {
 							<th>Password</th>
 						</tr>
 					</thead>
-					<tbody />
+					<tbody>
+						{data.map(item => (
+							<tr key={item.id}>
+								<td>{item.email}</td>
+								<td>{item.phone}</td>
+								<td>{item.password}</td>
+							</tr>
+						))}
+					</tbody>
 				</table>
 			</div>
 		</div>
