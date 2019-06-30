8818f93d66cf0c10931c9d1e6e5e4067efc77fac
Serge <protopulos.pulatikos@yandex.ru>
2019-06-30 11:48:23 +0300

    footer introduced

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
 	return (
-		<footer className="border">
-			<h2 className="text-center">Footer</h2>
+		<footer className="footer">
+			<div className="flex-container">
+				<div className="left">
+					<p>Transporters {new Date().getFullYear()}</p>
+				</div>
+				<div className="right">
+					<p>Test task</p>
+				</div>
+			</div>
 		</footer>
 	);
 };
